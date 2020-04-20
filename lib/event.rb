cd1209f1c57a4a31c36430ac7ffe70c5077a723d
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-04-20 17:01:41 +0200

    feat: event & user

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,47 @@
-class Event
+require 'time'
 
+class Event
+  attr_accessor :start_date, :duration, :title, :attendees
+
+  def initialize(start_date, duration, title, attendees)
+    @start_date = Time.parse(start_date)
+    @duration = duration
+    @title = title
+    @attendees = attendees
+  end
+
+  def postpone_24h
+    @start_date = @start_date + 86400
+  end
+
+  def end_date
+    @start_date + @duration
+  end
+
+  def is_past?
+    @start_date < Time.now
+  end
+
+  def is_futur?
+    @start_date > Time.now
+  end
+
+  def is_soon
+    if @start_date.day == Time.now.day && (@start_date.min - Time.now.min) <= 30
+      return true 
+    else
+      return false
+    end
+  end
+
+  def to_s
+    puts "> Titre : #{@title}"
+    puts "> Date de début : #{@start_date}"
+    puts "> Durée : #{@duration}"
+    puts "> Invités : #{@attendees.join(", ")}"
+  end
 end
+
+
+
+
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,24 @@
-class User
+require 'pry'
 
+class User
+  attr_accessor :email, :age
+  @@all_users = []
+
+  def initialize(email, age)
+    @email = email
+    @age = age
+    @@all_users << self
+  end
+
+  def self.all
+    return @@all_users
+  end
+
+  def self.find_by_email(email)
+    @@all_users.each do |user|
+      if user.email == email
+        return user
+      end
+    end
+  end
 end
